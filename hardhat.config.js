0d569e6e9dcc2693af304b8a9c57d56a5c731ac6
abzaliito <bakhytov2000@bk.ru>
2026-02-07 17:06:06 +0500

    123

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,14 +1,12 @@
 require("@nomicfoundation/hardhat-toolbox");
-require("dotenv").config(); // Чтобы читать ключи из .env
+require("dotenv").config(); 
 
 module.exports = {
-  solidity: "0.8.20", // Убедись, что версия совпадает с твоими .sol файлами
+  solidity: "0.8.20", 
   networks: {
-    // Для тестов на твоем компе
     localhost: {
       url: "http://127.0.0.1:8545"
     },
-    // Пример настройки для тестовой сети (например, Amoy/Polygon)
     amoy: {
       url: process.env.RPC_URL || "",
       accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : []
