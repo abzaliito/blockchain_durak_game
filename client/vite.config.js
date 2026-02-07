b15807be2edc38585cf8b2799c0a960b2b45c4bf
evelina <evelina.penkova0@gmail.com>
2026-02-07 19:15:33 +0500

    fix

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, '../', '')
+  
+  return {
+    plugins: [react()],
+    define: {
+      __CHIPS_ADDRESS__: JSON.stringify(env.CHIPS_ADDRESS),
+      __XP_ADDRESS__: JSON.stringify(env.XP_ADDRESS),
+      __LOBBY_ADDRESS__: JSON.stringify(env.LOBBY_ADDRESS),
+      __RPC_URL__: JSON.stringify(env.RPC_URL || 'http://127.0.0.1:8545'),
+      __CHAIN_ID__: JSON.stringify(env.CHAIN_ID || '31337'),
+    }
+  }
 })
